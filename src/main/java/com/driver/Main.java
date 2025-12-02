6dd9dbdc3260fe2a4780c50ebae6f1ebf07015e7
sufiyan27492 <sayasi5445@gmail.com>
2025-12-02 21:20:56 +0530

    Added polymorphism Product assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,32 @@
 package com.driver;
 
 public class Main {
-    public static class product{
-        public int p(int x, int y) {
+    public static class Product {
+
+        public int product(int x, int y) {
             return x * y;
         }
-        public int p(int x, int y, int z) {
+
+        public int product(int x, int y, int z) {
             return x * y * z;
         }
-        public double p(double x, double y) {
+
+        public double product(double x, double y) {
             return x * y;
         }
     }
 
     public static void main(String[] args) {
-        product p = new product();
-        System.out.println(p.p(4,5));
-        System.out.println(p.p(7,7,4));
-        System.out.println(p.p(4.6,7.5));
-    }
 
+        Product p = new Product();
+
+        // Task 3
+        System.out.println(p.product(1, 2));
+
+        // Task 4
+        System.out.println(p.product(1, 2, 3));
+
+        // Task 5
+        System.out.println(p.product(1.5, 2.5));
+    }
 }
